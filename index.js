156d916fe320ce5c834664cfc2dbbc4d8ed63e17
Cete4 <46322113+Cete4@users.noreply.github.com>
2022-01-01 21:30:16 -0600

    'commit'

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ import {exec} from 'child_process';
 const logger = signale;
 
 logger.start("--------------- Script start ---------------")
+
 for(let month = 1; month < 2; month++){
     for(let day = 1; day < 28; day++){
         setTimeout(async () => {
-            await console.log("Waiting for calls to complete");
-        }, 5000)
-        exec(`git add . && git commit --date '2022-${month}-${day}' -m 'commit' && git push`, (error, stdout, stderr) => {
-            if (error) {
-                logger.error(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                logger.error(`stderr: ${stderr}`);
-                return;
-            }
-            logger.awaiting(`\nstdout: ${stdout}`);
-            logger.complete(`Commited to 2022-${month}-${day}`)
-        });
+            exec(`git add . && git commit --date '2022-${month}-${day}' -m 'commit' && git push`, (error, stdout, stderr) => {
+                if (error) {
+                    logger.error(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    logger.error(`stderr: ${stderr}`);
+                    return;
+                }
+                logger.awaiting(`\nstdout: ${stdout}`);
+                logger.complete(`Commited to 2022-${month}-${day}`)
+            });
 
+        }, 5000)
     }
 }
 
