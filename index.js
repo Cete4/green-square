3e46c9278909047c05670ce7da3b3a5bd9fa299b
Cete4 <46322113+Cete4@users.noreply.github.com>
2022-01-01 21:26:51 -0600

    'commit'

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ for(let month = 1; month < 2; month++){
     for(let day = 1; day < 28; day++){
 
         exec(`git add . && git commit --date '2022-${month}-${day}' -m 'commit' && git push`, (error, stdout, stderr) => {
-            setTimeout(() => {
-                logger.awaiting("Waiting for calls to comlete");
-            }, "5000")
             if (error) {
                 logger.error(`error: ${error.message}`);
                 return;
@@ -22,6 +19,9 @@ for(let month = 1; month < 2; month++){
             logger.awaiting(`\nstdout: ${stdout}`);
             logger.complete(`Commited to 2022-${month}-${day}`)
         });
+        setTimeout(() => {
+            logger.awaiting("Waiting for calls to complete");
+        }, 5000)
     }
 }
 
