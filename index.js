02999b9e557d034275f7cc8139c324736b8b6e01
Cete4 <46322113+Cete4@users.noreply.github.com>
2023-02-27 22:05:59 -0600

    Got the script to psuedo randomly commit on each day starting in 2020

--- index.js
+++ index.js
@@ -5,20 +5,41 @@ const logger = signale;
 
 logger.start("--------------- Script start ---------------")
 
-let month = 3
-let day = 22
+let month = 1
+let day = 1
+let year = 2020
+
+setInterval(commit, 2000)
+
+function commit(){
+    if(day === 31) {
+        day = 1
+        month++
+    } else {
+        day++
+    }
+    if(month === 13){
+        year++;
+        month = 1
+    }
+    if(year === 2023){
+        process.exit(0)
+    }
+    day % Math.floor(Math.random() * 3) === 0 ?
+    exec(`git commit --allow-empty --date '${year}-${month}-${day}' -m 'New' && git push`, (error, stdout, stderr) => {
+        if (error) {
+            logger.error(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            logger.error(`stderr: ${stderr}`);
+            return;
+        }
+        logger.awaiting(`\nstdout: ${stdout}`);
+        logger.complete(`Commited to ${year}-${month}-${day}`)
+    }) : logger.debug(`Didn't commit on ${year}-${month}-${day}`)
+}
 
 
-exec(`git add . && git commit --date '2022-${month}-${day}' -m 'commit' && git push`, (error, stdout, stderr) => {
-    if (error) {
-        logger.error(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        logger.error(`stderr: ${stderr}`);
-        return;
-    }
-    logger.awaiting(`\nstdout: ${stdout}`);
-    logger.complete(`Commited to 2022-${month}-${day}`)
-});
+
 
